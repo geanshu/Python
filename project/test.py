18a5056a06990de0dda09d5e16a06ddc3caa3d7a
阿博-斯 <aolong0714@qq.com>
2020-09-11 15:19:29 +0800

    火焰魅影后台发送

--- project/test.py
+++ project/test.py
@@ -1,18 +1,32 @@
 import json
 import requests
 import sqlite3
+import time
 
+def main():
+    conn = sqlite3.connect('project/test.db')
+    c = conn.cursor()
+    print("Opened database successfully")
+    c.execute("SELECT testid, pcount  from people_count where testid='4' ")
+    before=c.fetchall()
 
-conn = sqlite3.connect('project/test.db')
-c = conn.cursor()
-print("Opened database successfully")
-cursor = c.execute("SELECT testid, pcount  from people_count")
-for row in cursor:
-    print("ID = ", row[0],"    count = ", row[1])
+    while True:
+        time.sleep(1)
+        c.execute("SELECT testid, pcount  from people_count where testid='4' ")
+        after=c.fetchall()
+        if len(after<before):
+            continue
+        change=after[len(before):]
 
-print("Operation done successfully")
-conn.close()
-# return HttpResponse(json.dumps{
-#     'id': id,
-#     'count': count
-# }))
+        for row in change:
+            print("Changed:ID = ", row[0], "    count = ", row[1])
+
+            requests.post(url='http://localhost:8080/Fire/', data=json.dumps({
+                'id': row[0],
+                'count': row[1]
+            }), headers={'content_type': 'application/json'})
+        before=after
+    conn.close()
+
+if __name__ == "__main__":
+    main()

afc3545818f0e97c371db5cf5ff57703160101f7
阿博-斯 <aolong0714@qq.com>
2020-09-11 16:14:39 +0800

    焰影神探

--- project/test.py
+++ project/test.py
@@ -10,23 +10,32 @@ def main():
     c.execute("SELECT testid, pcount  from people_count where testid='4' ")
     before=c.fetchall()
 
+    f=open('project/a123.json', mode='w')
+    f.write('{"id": 3, "count": 3}')
+    f.close()
     while True:
         time.sleep(1)
-        c.execute("SELECT testid, pcount  from people_count where testid='4' ")
-        after=c.fetchall()
-        if len(after<before):
-            continue
-        change=after[len(before):]
+        
+        # requests.post(url='http://localhost:8080/Fire/a123', data=json.dumps({
+        #     'id': 3,
+        #     'count': 3
+        # }), headers={'content_type': 'application/json'})
 
-        for row in change:
-            print("Changed:ID = ", row[0], "    count = ", row[1])
+    #     c.execute("SELECT testid, pcount  from people_count where testid='4' ")
+    #     after=c.fetchall()
+    #     if len(after<before):
+    #         continue
+    #     change=after[len(before):]
 
-            requests.post(url='http://localhost:8080/Fire/', data=json.dumps({
-                'id': row[0],
-                'count': row[1]
-            }), headers={'content_type': 'application/json'})
-        before=after
-    conn.close()
+    #     for row in change:
+    #         print("Changed:ID = ", row[0], "    count = ", row[1])
 
+    #         requests.post(url='http://localhost:8080/Fire/a123.json', data=json.dumps({
+    #             'id': row[0],
+    #             'count': row[1]
+    #         }), headers={'content_type': 'application/json'})
+    #     before=after
+    # conn.close()
+    
 if __name__ == "__main__":
     main()
